6f833b8fa5911930b7d92b63c377f71b645c232d
Alex Thewsey <athewsey@cantab.net>
2017-07-16 17:00:51 +0100

    First d3 line

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import {
 } from '@angular/core';
 
 import { AppState } from '../app.service';
+import { ScaleLinear, scaleLinear } from "d3-scale";
+import { select as d3select, ValueFn } from "d3-selection";
+import { Line, line } from "d3-shape";
 
 @Component({
   /**
@@ -26,6 +29,10 @@ import { AppState } from '../app.service';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  width: number = 400;
+  height: number = 200;
+  scaleX: ScaleLinear<number, number> = scaleLinear();
+  scaleY: ScaleLinear<number, number> = scaleLinear();
   /**
    * Set our default values
    */
@@ -38,7 +45,25 @@ export class HomeComponent implements OnInit {
   ) {}
 
   public ngOnInit() {
-    console.log('hello `Home` component');
+    const me = this;
+    const graphEl = document.getElementById("graph");
+    graphEl.style.width = `${this.width}px`;
+    graphEl.style.height = `${this.height}px`;
+    this.scaleX.domain([0, 1]).range([0, this.width]);
+    this.scaleY.domain([0, 1]).range([0, this.height]);
+    const valLine: Line<any> = line()
+      .x((d: any) => (me.scaleX(d.x)))
+      .y((d: any) => (me.scaleY(d.y)));
+    const svg = d3select(graphEl);
+    const grp = svg.append("g");
+    const ln = grp.append("path").attr("class", "line");
+    // Calling .data gives a "parent is null" error?
+    //ln.data([{ x: 0, y: 1 }, { x: 1, y: 0 }])
+    //  .attr("d", valLine as ValueFn<any, any, string>);
+    ln.attr("d", valLine([{ x: 0, y: 1 }, { x: 1, y: 0 }]))
+      .attr("stroke", "blue")
+      .attr("stroke-width", "2");
+    console.log();
   }
 
   public submitState(value: string) {
