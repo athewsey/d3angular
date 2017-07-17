e17de2af97e1bc4f1449af556539ecca257271e1
Alex Thewsey <athewsey@cantab.net>
2017-07-17 01:41:44 +0100

    Layered gauss blurs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,100 @@ import {
   OnInit
 } from '@angular/core';
 
-import { AppState } from '../app.service';
+import { ViewEncapsulation } from "@angular/core";
 import { ScaleLinear, scaleLinear } from "d3-scale";
 import { select as d3select, ValueFn } from "d3-selection";
 import { Line, line } from "d3-shape";
+import { AppState } from '../app.service';
+
+const SCALE: number = 2;
+const STEPSPERPX: number = 2;
+
+class DiscreteGaussFilter {
+  /**
+   * Gaussian exponential factor relating x position to std deviation
+   */
+  expFactor: number;
+
+  /**
+   * Number of samples to each side of the central sample
+   */
+  nSideSamples: number;
+
+  /**
+   * Coefficient[AvailablePre][AvailablePost][ix]
+   */
+  coeffs: Array<Array<Array<number>>>;
+
+  /**
+   * 
+   * @param stddev 
+   * @param precision 
+   */
+  constructor(stddev, precision) {
+    const me = this;
+    this.expFactor = -1 / (2 * stddev ** 2);
+    this.nSideSamples = Math.max(1, Math.round(stddev * precision));
+
+    // Exponential coefficients for the forward half of the filter (symmetric):
+    // Array.apply.map to initialise array of given size
+    const halfExps = Array.apply(
+      null,
+      { length: this.nSideSamples }
+    ).map((nothing, ix) => (
+      Math.exp(me.expFactor * (ix + 1) ** 2)
+    ));
+
+    // Cumulative sum of this and following halfExps.
+    const halfSums = halfExps.reduce(
+      (acc: Array<number>, next: number, ix: number) => {
+        // [][-1] = undefined, no error
+        acc.push(next + (acc[ix - 1] || 0));
+        return acc;
+      },
+      []
+    );
+
+    this.coeffs = Array.apply(
+      null,
+      { length: this.nSideSamples + 1 }
+    ).map((nothing, ixAvailPre) => (
+      Array.apply(
+        null,
+        { length: this.nSideSamples + 1 }
+      ).map((nothing, ixAvailPost) => {
+        const coeffSum = (halfSums[ixAvailPre - 1] || 0) + 1 + (halfSums[ixAvailPost - 1] || 0);
+        return halfExps
+          .slice(0, ixAvailPre)
+          .reverse()
+          .concat(1, halfExps.slice(0, ixAvailPost))
+          .map((d) => (d / coeffSum));
+      })
+    ));
+  }
+
+  filter(input: Array<number>): Array<number> {
+    const me = this;
+    const len = input.length;
+    return input.map((d, ix) => {
+      const availablePre = Math.min(ix, me.nSideSamples);
+      const availablePost = Math.min(len - (ix + 1), me.nSideSamples);
+      const coeffs = me.coeffs[availablePre][availablePost];
+      return input.slice(
+        ix - availablePre,
+        ix + availablePost + 1
+      ).reduce((acc, nextIn, ix) => (
+        acc + nextIn * coeffs[ix]
+      ), 0);
+    });
+  }
+}
 
 @Component({
+  /**
+   * TODO: A way to accommodate d3 dynamic DOM without breaking ViewEncapsulation
+   */
+  encapsulation: ViewEncapsulation.None,
   /**
    * The selector is what angular internally uses
    * for `document.querySelectorAll(selector)` in our index.html
@@ -29,8 +117,8 @@ import { Line, line } from "d3-shape";
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-  width: number = 400;
-  height: number = 200;
+  width: number = (202 - 150);
+  height: number = (250 - 15);
   scaleX: ScaleLinear<number, number> = scaleLinear();
   scaleY: ScaleLinear<number, number> = scaleLinear();
   /**
@@ -44,26 +132,41 @@ export class HomeComponent implements OnInit {
     public appState: AppState
   ) {}
 
+
+  public getPlotData() {
+    const nSteps: number = this.height * STEPSPERPX;
+    const time = Array.apply(null, { length: nSteps }).map((d, ix) => (ix / STEPSPERPX));
+    const input = time.map((t) => (Math.random() * this.width));
+    const gaussFilters = [
+      new DiscreteGaussFilter(1.5, 4),
+      new DiscreteGaussFilter(3, 4),
+      new DiscreteGaussFilter(6, 4)
+    ];
+    const blurred = gaussFilters.map((f) => (f.filter(input)));
+    return [
+      time.map((t, ix) => ({ x: input[ix], y: t }))
+    ].concat(blurred.map((output) => (
+      output.map((x, ix) => ({ x: x, y: time[ix] }))
+    )));
+  }
+
   public ngOnInit() {
     const me = this;
+    const plotData = this.getPlotData();
     const graphEl = document.getElementById("graph");
-    graphEl.style.width = `${this.width}px`;
-    graphEl.style.height = `${this.height}px`;
-    this.scaleX.domain([0, 1]).range([0, this.width]);
-    this.scaleY.domain([0, 1]).range([0, this.height]);
+    graphEl.style.width = `${this.width * SCALE}px`;
+    graphEl.style.height = `${this.height * SCALE}px`;
+    this.scaleX.domain([0, this.width]).range([0, this.width * SCALE]);
+    this.scaleY.domain([0, this.height]).range([0, this.height * SCALE]);
     const valLine: Line<any> = line()
       .x((d: any) => (me.scaleX(d.x)))
       .y((d: any) => (me.scaleY(d.y)));
     const svg = d3select(graphEl);
     const grp = svg.append("g");
-    const ln = grp.append("path").attr("class", "line");
-    // Calling .data gives a "parent is null" error?
-    //ln.data([{ x: 0, y: 1 }, { x: 1, y: 0 }])
-    //  .attr("d", valLine as ValueFn<any, any, string>);
-    ln.attr("d", valLine([{ x: 0, y: 1 }, { x: 1, y: 0 }]))
-      .attr("stroke", "blue")
-      .attr("stroke-width", "2");
-    console.log();
+    plotData.forEach((series, ix) => {
+      grp.append("path").attr("class", `line line-${ix}`)
+        .attr("d", valLine(series));
+    });
   }
 
   public submitState(value: string) {
